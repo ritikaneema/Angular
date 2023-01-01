b6995cdfc34604d7dd7649249379d603cce1b4eb
ritikaneema <65679700+ritikaneema@users.noreply.github.com>
2023-01-02 00:27:38 +0530

    Add files via upload

---
+++ src_StepperAssignment/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import AppComponent from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './MaterialModule';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  declarations: [AppComponent, RegistrationComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
